21e3d9cb93c7260a20067529591fe47532b0c5ee
GitHub Action <action@github.com>
2022-02-06 20:38:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/clojj/ijplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/clojj/ijplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.clojj.ijplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.clojj.ijplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/clojj/ijplugintemplate/MyPluginTest.kt
+++ src/test/kotlin/com/github/clojj/ijplugintemplate/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.clojj.ijplugintemplate
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
